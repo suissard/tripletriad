04b7b15ca420fe3c0f71a6dfb1a24a0b390cba71
suissard <clavier.jeremy@gmail.com>
2026-05-01 11:03:14 +0200

    chore: remove temporary scratch scripts and build artifacts

--- scratch/skip_steps.js
+++
@@ -1,1 +0,0 @@
-// Steps 1, 2, and 3 are already done as they were completed in the previous prompt iteration.
--- scratch/verify_build.cjs
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  execSync('cd back/strapi && npm run build -- --no-optimization', { stdio: 'inherit' });
-} catch (e) {
-  console.log("Failed to build");
-}
